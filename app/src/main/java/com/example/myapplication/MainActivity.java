ad52a4d623f06efd3ad3d47e14cbfa023d683a37
Lenovo <eahmatteshmar@gmail.com>
2022-12-28 17:23:02 +0500

    asasas

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,18 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import android.content.Intent;
 import android.hardware.lights.LightState;
 import android.os.Bundle;
+import android.service.controls.actions.FloatAction;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fab=findViewById(R.id.floatActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,CameraActivity.class));
+            }
+        });
 
         RecyclerView recyclerView=findViewById(R.id.main_container);
         recyclerView.setHasFixedSize(true);
